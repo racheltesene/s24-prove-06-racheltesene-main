343c4b556e882adb90d177741f0b2a177d8b49ae
racheltesene <tes21002@byui.edu>
2024-05-31 15:11:17 -0600

    yes

--- Translator.cs
+++ Translator.cs
@@ -1,28 +1,41 @@
-namespace prove_06;
+using System.Collections;
+using System.Collections.Generic;
 
-public class Translator {
-    private Dictionary<string, string> _words = new();
-
-    /// <summary>
-    /// Add the translation from 'from_word' to 'to_word'
-    /// For example, in a english to german dictionary:
-    /// 
-    /// my_translator.AddWord("book","buch")
-    /// </summary>
-    /// <param name="fromWord">The word to translate from</param>
-    /// <param name="toWord">The word to translate to</param>
-    /// <returns>fixed array of divisors</returns>
-    public void AddWord(string fromWord, string toWord) {
-        // Todo Problem 1 - ADD YOUR CODE HERE
+namespace prove_06
+{
+    public static class DictionaryExtensionMethods
+    {
+        public static string AsString(this IEnumerable dictionary)
+        {
+            return "<Dictionary>{" + string.Join(", ", (Dictionary<string, string>)dictionary) + "}";
+        }
     }
 
-    /// <summary>
-    /// Translates the from word into the word that this stores as the translation
-    /// </summary>
-    /// <param name="fromWord">The word to translate</param>
-    /// <returns>The translated word or "???" if no translation is available</returns>
-    public string Translate(string fromWord) {
-        // Todo Problem 1 - ADD YOUR CODE HERE
-        return "";
+    public class Translator
+    {
+        private Dictionary<string, string> _words = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Add the translation from 'fromWord' to 'toWord'
+        /// For example, in an English to German dictionary:
+        /// 
+        /// myTranslator.AddWord("book","buch")
+        /// </summary>
+        /// <param name="fromWord">The word to translate from</param>
+        /// <param name="toWord">The word to translate to</param>
+        public void AddWord(string fromWord, string toWord)
+        {
+            _words[fromWord] = toWord;
+        }
+
+        /// <summary>
+        /// Translates the from word into the word that this stores as the translation
+        /// </summary>
+        /// <param name="fromWord">The word to translate</param>
+        /// <returns>The translated word or "???" if no translation is available</returns>
+        public string Translate(string fromWord)
+        {
+            return _words.TryGetValue(fromWord, out var translation) ? translation : "???";
+        }
     }
 }
