55b508a847cde3b068aa731a09dabda8bd5fcec4
racheltesene <tes21002@byui.edu>
2024-05-31 15:47:02 -0600

    final

--- Maze.cs
+++ Maze.cs
@@ -1,64 +1,52 @@
-namespace prove_06;
+// Maze.cs
+using System;
+using System.Collections.Generic;
 
-/// <summary>
-/// Defines a maze using a dictionary. The dictionary is provided by the
-/// user when the Maze object is created. The dictionary will contain the
-/// following mapping:
-///
-/// (x,y) : [left, right, up, down]
-///
-/// 'x' and 'y' are integers and represents locations in the maze.
-/// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
-///
-/// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
-///
-/// If there is a wall, then display "Can't go that way!".  If there is no wall,
-/// then the 'currX' and 'currY' values should be changed.
-/// </summary>
-public class Maze {
+public class Maze
+{
     private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
     private int _currX = 1;
     private int _currY = 1;
 
-    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap) {
+    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
+    {
         _mazeMap = mazeMap;
     }
 
-    // Todo Problem 4 - ADD YOUR CODE HERE
-    /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, then display "Can't go that way!"
-    /// </summary>
-    public void MoveLeft() {
-        // FILL IN CODE
+    public void MoveLeft()
+    {
+        if (_mazeMap[(_currX, _currY)][0])
+            _currY--;
+        else
+            Console.WriteLine("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, then display "Can't go that way!"
-    /// </summary>
-    public void MoveRight() {
-        // FILL IN CODE
+    public void MoveRight()
+    {
+        if (_mazeMap[(_currX, _currY)][1])
+            _currY++;
+        else
+            Console.WriteLine("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, then display "Can't go that way!"
-    /// </summary>
-    public void MoveUp() {
-        // FILL IN CODE
+    public void MoveUp()
+    {
+        if (_mazeMap[(_currX, _currY)][2])
+            _currX--;
+        else
+            Console.WriteLine("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, then display "Can't go that way!"
-    /// </summary>
-    public void MoveDown() {
-        // FILL IN CODE
+    public void MoveDown()
+    {
+        if (_mazeMap[(_currX, _currY)][3])
+            _currX++;
+        else
+            Console.WriteLine("Can't go that way!");
     }
 
-    public void ShowStatus() {
+    public void ShowStatus()
+    {
         Console.WriteLine($"Current location (x={_currX}, y={_currY})");
     }
 }
--- Translator.cs
+++ Translator.cs
@@ -3,39 +3,25 @@ using System.Collections.Generic;
 
 namespace prove_06
 {
-    public static class DictionaryExtensionMethods
-    {
-        public static string AsString(this IEnumerable dictionary)
-        {
-            return "<Dictionary>{" + string.Join(", ", (Dictionary<string, string>)dictionary) + "}";
-        }
-    }
-
     public class Translator
     {
-        private Dictionary<string, string> _words = new Dictionary<string, string>();
+        private Dictionary<string, string> _translations = new Dictionary<string, string>();
 
-        /// <summary>
-        /// Add the translation from 'fromWord' to 'toWord'
-        /// For example, in an English to German dictionary:
-        /// 
-        /// myTranslator.AddWord("book","buch")
-        /// </summary>
-        /// <param name="fromWord">The word to translate from</param>
-        /// <param name="toWord">The word to translate to</param>
         public void AddWord(string fromWord, string toWord)
         {
-            _words[fromWord] = toWord;
+            _translations[fromWord.ToLower()] = toWord.ToLower();
         }
 
-        /// <summary>
-        /// Translates the from word into the word that this stores as the translation
-        /// </summary>
-        /// <param name="fromWord">The word to translate</param>
-        /// <returns>The translated word or "???" if no translation is available</returns>
-        public string Translate(string fromWord)
+        public string Translate(string word)
         {
-            return _words.TryGetValue(fromWord, out var translation) ? translation : "???";
+            if (_translations.ContainsKey(word.ToLower()))
+            {
+                return _translations[word.ToLower()];
+            }
+            else
+            {
+                return "???";
+            }
         }
     }
 }
